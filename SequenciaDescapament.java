1862872e495cfa141fdfe0c34a8988ae40be46f1
mocinnajm <alge1257@gmail.com>
2024-11-10 19:54:01 +0100

    TascaSequenciaDescapament

---
+++ SequenciaDescapament.java
@@ -0,0 +1,11 @@
+class SequenciaDescapament {
+    public static void main(String[] args) {
+        System.out.println("\t Futbol");
+        System.out.println(" Club");
+        System.out.println("\t Barcelona");
+        System.out.println(" 1899");
+        }
+        }
+
+
+        
